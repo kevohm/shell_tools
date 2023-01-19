f7a65933abf76cdcef3be289b065cb2262e93b3b
kevohm <kevinkbet@gmail.com>
2023-01-19 10:28:48 +0000

    Added git config

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 MAIN_BIN=/usr/bin/
 if sudo -v;then
 	printf "\n************ installing *************\n"
-	printf "1). push\n2). parsecode\n\n"
-	sudo cp push parsecode $MAIN_BIN
+	printf "1). push\n2). parsecode\n3). setup_git\n\n"
+	sudo cp push parsecode setup_git $MAIN_BIN
 else
 	echo You are not root!
 
